1ab6761df0657f44a0b050bd6386b624e2c7784e
Oskar Stenberg <01ste02@gmail.com>
2019-03-15 12:43:49 +0100

    Add project files.

---
+++ Övning 12.2/DrawDiagram.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+namespace Övning_12._2
+{
+    class DrawDiagram
+    {
+
+        public DrawDiagram()
+        {
+
+        }
+
+        public void Paint (int[] values, int maxValue, Graphics g)
+        {
+            int baseY = 50;
+            g.DrawLine(Pens.Black, baseY, baseY, baseY, baseY + 2 + 2*maxValue);
+            g.DrawLine(Pens.Black, baseY, baseY + 2 + 2*maxValue, baseY + 2* values.Length + 2, baseY + 2 + 2*maxValue);
+
+
+            int xCoord = baseY + 1;
+            int YBase = baseY + 2 + 2*maxValue;
+            for (int i = 0; i < values.Length; i++)
+            {
+                g.DrawRectangle(Pens.Red, xCoord, YBase - 2*values[i], 1, 1);
+                xCoord += 2;
+            }
+        }
+    }
+}
---
+++ Övning 12.2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Övning_12._2
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        private GenerateNumbers genNum = new GenerateNumbers();
+        private DrawDiagram diagram = new DrawDiagram();
+        private Bubblesort sort = new Bubblesort();
+
+        private int[] values;
+        private int minValue = 0;
+        private int maxValue = 100;
+        private int numValues = 200;
+
+        public Form1 ()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click (object sender, EventArgs e)
+        {
+            values = genNum.Generate(random, numValues, minValue, maxValue);
+            btnSort.Enabled = true;
+            Invalidate();
+        }
+
+        private void btnSort_Click (object sender, EventArgs e)
+        {
+            values = sort.Sort(values);
+            Invalidate();
+        }
+
+        protected override void OnPaint (PaintEventArgs e)
+        {
+            if (values != null)
+            {
+                diagram.Paint(values, maxValue, e.Graphics);
+            }
+        }
+    }
+}
